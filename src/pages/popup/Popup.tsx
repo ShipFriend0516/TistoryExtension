df89b5664f6b8be231df559613b3d98836e4c52f
Oracionspace <shipfriend4371@naver.com>
2024-02-23 00:16:43 +0900

    feat: 개발환경세팅 및 초기화면 구축

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,43 +1,63 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React, { useState } from 'react';
 import '@pages/popup/Popup.css';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
+  const [selectedTab, setSelectedTab] = useState(0);
+  const tabList = ['기능 목록', '단축키 설정', '크레딧'];
+  const funcList = ['추가 단축키 활성화', '이미지 대체텍스트 입력기', '페이지 미리보기'];
+  // const [checkedList, setCheckedList] = useState([]);
 
+  const onClickCheckBox = (index: number, isChecked: boolean): void => {
+    // const funcKey = `func_${index}`;
+    chrome.storage.local
+      .set({
+        funcKey: isChecked,
+      })
+      .then(res => {
+        console.log(res);
+      });
+  };
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: theme === 'light' && '#0281dc', marginBottom: '10px' }}>
-          Learn React!
+    <main className="popup-container">
+      <h1>Story Helper</h1>
+      <p>티스토리 글쓰기를 더 편하게</p>
+      <div className="tab">
+        {tabList.map((tab, i) => (
+          <button
+            onClick={() => {
+              setSelectedTab(i);
+            }}
+            className={`${selectedTab === i && 'selected'}`}
+            key={i}>
+            {tab}
+          </button>
+        ))}
+      </div>
+      <ul className="funcList">
+        {funcList.map((func, i) => {
+          return (
+            <li key={i}>
+              <input
+                type="checkbox"
+                onChange={e => {
+                  console.log('체크됨');
+                  onClickCheckBox(i, e.target.checked);
+                }}
+              />
+              <span>{func}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="btnWrapper">
+        <button>단축키 설정</button>
+        <a href="https://github.com/ShipFriend0516" target="_blank" rel="noreferrer">
+          <button>깃허브</button>
         </a>
-        <button
-          style={{
-            backgroundColor: theme === 'light' ? '#fff' : '#000',
-            color: theme === 'light' ? '#000' : '#fff',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme
-        </button>
-      </header>
-    </div>
+      </div>
+    </main>
   );
 };
 
