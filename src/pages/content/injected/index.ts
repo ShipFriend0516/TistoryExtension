7fbbf45a3cd37ead1210784efe15cd72bf60d62d
Oracionspace <shipfriend4371@naver.com>
2024-06-07 22:53:58 +0900

    feat: 단축키 추가기능 비활성화

--- src/pages/content/injected/index.ts
+++ src/pages/content/injected/index.ts
@@ -1,5 +1,5 @@
 import('@pages/content/injected/altTager');
-import('@pages/content/injected/keymap');
+// import('@pages/content/injected/keymap');
 import('@pages/content/injected/imageSize');
 
 console.log('index.ts 로드 완료');
